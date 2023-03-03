9d10347eb40547095af4448f8dd2f471be4b523d
preetham-coupa <preetham.p-m@coupa.com>
2023-03-03 19:30:55 +0530

    First Commit

---
+++ src/Acme.BookStore.EntityFrameworkCore/Migrations/20221125102429_Added_Data_To_Books.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Acme.BookStore.Migrations
+{
+    public partial class Added_Data_To_Books : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
